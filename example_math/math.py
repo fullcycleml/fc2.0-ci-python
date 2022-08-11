1bf4ac92f691986f4fd560192585ce35e7cffc79
Marlon Leite <marlonleite@gmail.com>
2022-08-11 11:10:05 -0300

    feat: add function sum

---
+++ example_math/math.py
@@ -0,0 +1,7 @@
+def soma(a: int, b: int) -> int:
+    return a + b
+
+def main() -> int:
+    return soma(10, 10)
+
+main()
---
+++ tests/test_math.py
@@ -0,0 +1,5 @@
+from example_math.math import soma
+
+
+def test_soma():
+    assert soma(15, 15) == 30
